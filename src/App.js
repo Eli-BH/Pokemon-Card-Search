7e3c5eb5e774613c7698c4a0ad9fe67a75e9af87
Eli-b-good <eli.i.henderson95@gmail.com>
2021-05-03 00:29:41 -0400

    Added react-card-flip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import ReactCardFlip from "react-card-flip";
 
 import SearchBar from "./components/SearchBar";
 import Header from "./images/Pokemon-Card-Search.png";
@@ -11,6 +12,7 @@ const App = () => {
   const [pokemon, setPokemon] = useState(null);
   const [move, setMove] = useState(null);
   const [ability, setAbility] = useState(null);
+  const [isFlipped, setIsFlipped] = useState(true);
 
   const submitSearch = async (e) => {
     e.preventDefault();
@@ -50,79 +52,105 @@ const App = () => {
         submitSearch={submitSearch}
       />
 
-      <div className="card-container">
-        <div className="card-face">
-          <div className="card-header">
-            <h2 className="pokemon-name">{pokemon?.name}</h2>
-          </div>
-
-          <div className="card-img-container">
-            <div className="card-img">
-              <img
-                src={pokemon?.sprites?.other["official-artwork"].front_default}
-                alt={pokemon?.name}
-              />
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <div
+          className={`card-container`}
+          onClick={() => setIsFlipped(!isFlipped)}
+        >
+          <div
+            className={`card-face background-${pokemon?.types[0].type.name}`}
+          >
+            <div className="card-header">
+              <h2 className="pokemon-name">{pokemon?.name}</h2>
             </div>
 
-            <div className="pokemon-measurements">
-              <p>Height:{Math.floor(pokemon?.height * 3.937)} in</p>
-              <p>Weight: {Math.floor(pokemon?.weight * 0.220462)} lbs</p>
-            </div>
-          </div>
+            <div className="card-img-container">
+              <div className="card-img">
+                <img
+                  src={
+                    pokemon?.sprites?.other["official-artwork"].front_default
+                  }
+                  alt={pokemon?.name}
+                />
+              </div>
 
-          <div className="stats">
-            <div className="stats-left">
-              <p>
-                Type:
-                <strong> {pokemon?.types[0].type.name}</strong>
-                {pokemon?.types?.length > 1 &&
-                  ` - ${pokemon?.types[1].type.name}`}
-              </p>
-              <p>
-                Base xp:
-                <strong>{pokemon?.base_experience || "not yet known"}</strong>
-              </p>
-              <p>
-                Base Hp: <strong>{pokemon?.stats[0]?.base_stat}</strong>
-              </p>
-              <p>
-                Base Attack: <strong>{pokemon?.stats[1].base_stat}</strong>
-              </p>
+              <div className="pokemon-measurements">
+                <p>Height:{Math.floor(pokemon?.height * 3.937)} in</p>
+                <p>Weight: {Math.floor(pokemon?.weight * 0.220462)} lbs</p>
+              </div>
             </div>
 
-            <div className="stats-right">
-              <p>
-                Base Defence: <strong>{pokemon?.stats[2].base_stat}</strong>
-              </p>
-              <p>
-                Base Special ATK: <strong>{pokemon?.stats[3].base_stat}</strong>
-              </p>
-              <p>
-                Base Special DEF: <strong>{pokemon?.stats[4].base_stat}</strong>
-              </p>
-              <p>
-                Base Speed: <strong>{pokemon?.stats[5].base_stat}</strong>
-              </p>
+            <div className="card-content">
+              <div className="stats">
+                <div className="stats-left">
+                  <p>
+                    Type:
+                    <strong> {pokemon?.types[0].type.name}</strong>
+                    <span>
+                      {pokemon?.types?.length > 1 &&
+                        `|${pokemon?.types[1].type.name}`}
+                    </span>
+                  </p>
+                  <p>
+                    Base xp:
+                    <strong>
+                      {pokemon?.base_experience || "not yet known"}
+                    </strong>
+                  </p>
+                  <p>
+                    Base Hp: <strong>{pokemon?.stats[0]?.base_stat}</strong>
+                  </p>
+                  <p>
+                    Base Attack: <strong>{pokemon?.stats[1].base_stat}</strong>
+                  </p>
+                </div>
+
+                <div className="stats-right">
+                  <p>
+                    Base Defence: <strong>{pokemon?.stats[2].base_stat}</strong>
+                  </p>
+                  <p>
+                    Base Special ATK:{" "}
+                    <strong>{pokemon?.stats[3].base_stat}</strong>
+                  </p>
+                  <p>
+                    Base Special DEF:{" "}
+                    <strong>{pokemon?.stats[4].base_stat}</strong>
+                  </p>
+                  <p>
+                    Base Speed: <strong>{pokemon?.stats[5].base_stat}</strong>
+                  </p>
+                </div>
+              </div>
+
+              <div className="moves">
+                <p>
+                  Move: <strong>{move?.name}</strong> -{" "}
+                  {move?.effect_entries[0].effect}
+                </p>
+
+                <p className="abilities">
+                  Ability: <strong>{ability?.name}</strong> -
+                  {
+                    ability?.effect_entries.filter(
+                      (item) => item.language.name === "en"
+                    )[0].effect
+                  }
+                </p>
+              </div>
             </div>
-          </div>
-
-          <div className="moves">
-            <p>
-              Move: <strong>{move?.name}</strong> -{" "}
-              {move?.effect_entries[0].effect}
-            </p>
-
-            <p>
-              Ability: <strong>{ability?.name}</strong> -
-              {
-                ability?.effect_entries.filter(
-                  (item) => item.language.name === "en"
-                )[0].effect
-              }
-            </p>
           </div>
         </div>
-      </div>
+
+        <div
+          className="card-container-back"
+          onClick={() => setIsFlipped(!isFlipped)}
+        >
+          <div className="card-back">
+            <div className="card-content-back"></div>
+          </div>
+        </div>
+      </ReactCardFlip>
     </div>
   );
 };
